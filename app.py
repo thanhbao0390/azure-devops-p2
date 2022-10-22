98d0e8f114dd58a383d5522cd490e1fa19ff4291
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2022-10-22 12:11:51 +0700

    update

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except ErrorLogs:
+    except: # pylint: disable=W0702
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
