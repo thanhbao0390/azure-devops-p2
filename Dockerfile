969360ef5be72d9633b829003da0b3adf4b42adb
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2022-11-20 18:52:53 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM python:3.7.3-stretch
 WORKDIR /app
 
 # Copy source code to working directory
-COPY . app.py /app/
+COPY . /app/
 
 # Install packages from requirements.txt
 # hadolint ignore=DL3013
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -12,38 +12,61 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-
-    LOG.info("Scaling Payload: %s payload")
-    scaler = StandardScaler().fit(payload)
-    scaled_adhoc_predict = scaler.transform(payload)
+    
+    LOG.info(f"Scaling Payload: \n{payload}")
+    scaler = StandardScaler().fit(payload.astype(float))
+    scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home BaoNT1</h3>"
+    html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-## TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
-    # Performs an sklearn prediction
-    try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
-        LOG.info("JSON payload Error: %s json_payload")
-        return "Model not loaded"
-
+    """Performs an sklearn prediction
+        
+        input looks like:
+        {
+        "CHAS":{
+        "0":0
+        },
+        "RM":{
+        "0":6.575
+        },
+        "TAX":{
+        "0":296.0
+        },
+        "PTRATIO":{
+        "0":15.3
+        },
+        "B":{
+        "0":396.9
+        },
+        "LSTAT":{
+        "0":4.98
+        }
+        
+        result looks like:
+        { "prediction": [ <val> ] }
+        
+        """
+    
+    # Logging the input payload
     json_payload = request.json
-    LOG.info("JSON payload: %s json_payload", json_payload)
+    LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload", inference_payload)
+    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    # scale the input
     scaled_payload = scale(inference_payload)
+    # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    LOG.info("prediction value: %s ", prediction)
+    ## TO DO:  Log the output prediction value
+    LOG.info(f"prediction output: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # load pretrained model as clf
+    clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
